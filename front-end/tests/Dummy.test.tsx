1311e8bdae4626eb76e575cd3f4ce6a8e3dc9c84
swaroop0803 <gnanaswaroop2003@gmail.com>
2025-10-24 12:04:31 +0530

    another

---
+++ front-end/tests/Dummy.test.tsx
@@ -0,0 +1,19 @@
+import { it } from 'vitest'
+import { expect,describe } from 'vitest'
+
+
+describe('Dummy', () => {
+    it('should show the output correctly', () => {
+    expect(4+5).toBe(9)
+    })
+    it('should show the output correctly', () => {
+    expect(4-5).toBe(-1)
+    })
+    it('should show the output correctly', () => {
+    expect(4*5).toBe(20)
+    })
+    it('should show the output correctly', () => {
+    expect(4/5).toBe(0.8)
+    })
+    
+})
