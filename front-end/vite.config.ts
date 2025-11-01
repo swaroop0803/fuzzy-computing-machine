dc0354a1da1ab7243498e5f16d0cf9c324731774
swaroop0803 <gnanaswaroop2003@gmail.com>
2025-11-01 16:52:37 +0530

    egse

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fuzzy-computing-machine/app/",
+  base: "/fuzzy-computing-machine/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
